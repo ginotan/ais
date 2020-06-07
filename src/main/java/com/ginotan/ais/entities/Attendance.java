f85fd2b7d7b0d7b9e87ffae00547b1607bfa0f51
ginotan <ogitaka815@gmail.com>
2020-06-07 16:51:04 +0900

    create Attendance

---
+++ src/main/java/com/ginotan/ais/entities/Attendance.java
@@ -0,0 +1,11 @@
+package com.ginotan.ais.entities;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Attendance {
+  private String userId;
+  private LocalDate date;
+  private LocalTime startTime;
+  private LocalTime endTime;
+}
