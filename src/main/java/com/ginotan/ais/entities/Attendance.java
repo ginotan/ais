eeca0d36958dce632a006243fd47b82524ef9f03
ginotan <ogitaka815@gmail.com>
2020-06-14 15:01:45 +0900

    modify Attendance: add Builder annotation

--- src/main/java/com/ginotan/ais/entities/Attendance.java
+++ src/main/java/com/ginotan/ais/entities/Attendance.java
@@ -1,8 +1,11 @@
 package com.ginotan.ais.entities;
 
+import lombok.Builder;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@Builder
 public class Attendance {
   private String userId;
   private LocalDate date;
