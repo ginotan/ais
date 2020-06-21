6d6fcc46549ed31eef5813ab27dfd0a3f61d5587
ginotan <ogitaka815@gmail.com>
2020-06-21 13:13:34 +0900

    create schema.sql: Create test data storage table

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE attendances (
+    user_id CHAR(6) NOT NULL,
+    attendance_date DATE NOT NULL,
+    start_time TIME,
+    end_time TIME,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id,attendance_date)
+  );
